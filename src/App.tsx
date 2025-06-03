7326190652b036dcdd64bfcfd5b309be62507ecd
Hasna Salihi <salihihasna02@gmail.com>
2025-06-03 18:35:02 +0100

    Adding login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import { LoginPage } from './pages/Login';
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <Routes>
+      {/* Shared wrapper with Navbar, footer, etc. */}
+      <Route element={<LoginPage></LoginPage>} path='/'></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
